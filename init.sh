57ba9b01171e98cc99d812c171f819c1ce1c7c3b
RryLee <qq215672398@126.com>
2016-10-15 18:09:04 +0800

    Init

---
+++ init.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+rm -rf template
+
+git clone https://github.com/laravel/laravel
+
+mv laravel template
+
+cd template
+
+cp .env.example .env
+cp .env.example .env.testing.example
+
+cp ../shipit.sh shipit.sh
+cp ../after_shipit.sh after_shipit.sh
+cp ../gitlab.ci.yml gitlab.ci.yml
+cp ../composer.json composer.json
+
+composer install
+
+#cp ../app.php config/app.php
+#cp ../kernel.php app/Http/kernel.php
+
+#php artisan vendor:publish --provider="Lingxi\Hashids\HashidsServiceProvider"
+#php artisan vendor:publish --provider="Lingxi\BrowserDetect\BrowserDetectServiceProvide"
+#php artisan vendor:publish --provider="Lingxi\OptionsManager\Providers\OptionsManagerProvider"
+#php artisan vendor:publish --provider="Lingxi\Cookie\CookieServiceProvider"
+
+rm -rf vendor
+
+echo "test.php" >> .gitignore
+
+rm -rf .git
+
+touch test.php
+
+echo "Complete!!!"
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# 灵析 web 项目模板
+
+#### 依赖
+
+- api-authentication
+- hashids
+- new-cookie
+- options-manager
+- browser-detect
+- l5-repository
